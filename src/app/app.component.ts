817bec9fb441841f5be226e0b442b1d7112f597c
SmailMeziani <smeziani@bk-consulting.com>
2018-10-10 16:58:23 +0200

    :sparkles: [Items] setting up the data from the app.component to the item.components through assertions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'Mon premier post'
+  lorem = 'Sed tamen haec cum ita tutius observentur, quidam vigore artuum inminuto rogati ad nuptias ubi aurum dextris manibus cavatis offertur, inpigre vel usque Spoletium pergunt. haec nobilium sunt instituta.'
 
+	posts = [
+		{
+			title: 'Mon premier post',
+		  content: this.lorem,
+		  loveIts: 0,
+		  created_at: new Date()
+		},
+		{
+			title: 'Mon second post',
+		  content: this.lorem,
+		  loveIts: 0,
+		  created_at: new Date()
+		},
+		{
+			title: 'Encore un post',
+		  content: this.lorem,
+		  loveIts: 0,
+		  created_at: new Date()
+		},
+	]
 }
--- src/app/post-list-item-component/post-list-item-component.component.ts
+++ src/app/post-list-item-component/post-list-item-component.component.ts
@@ -7,15 +7,13 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PostListItemComponentComponent implements OnInit {
 
-	@Input() passingThis: string
+	@Input() title: string
+	@Input() content: string
+	@Input() loveIts: number
+	@Input() createdAt: Date
 
   constructor() { }
 
-	post = {
-		title: 'Titre',
-		content: 'Intellectum est enim mihi quidem in multis, et maxime in me ipso, sed paulo ante in omnibus, cum M. Marcellum senatui reique publicae concessisti, commemoratis praesertim offensionibus, te auctoritatem huius ordinis dignitatemque rei publicae tuis vel doloribus vel suspicionibus anteferre. Ille quidem fructum omnis ante actae vitae hodierno die maximum cepit, cum summo consensu senatus, tum iudicio tuo gravissimo et maximo. Ex quo profecto intellegis quanta in dato beneficio sit laus, cum in accepto sit tanta gloria.',
-		date: new Date(),
-	}
 
   ngOnInit() {
   }
