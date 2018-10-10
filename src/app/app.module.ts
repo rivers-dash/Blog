360132387b3abf82de539e82a1be9ac8b69e8d87
SmailMeziani <smeziani@bk-consulting.com>
2018-10-10 16:08:34 +0200

    :sparkles: [Posts] Including post components
    This commit adds code for including static post component with like and dislike buttons that dont work for the moment, Also the date is set in place and so are the spaces between components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostListComponentComponent } from './post-list-component/post-list-component.component';
+import { FormsModule } from '@angular/forms';
+import { PostListItemComponentComponent } from './post-list-item-component/post-list-item-component.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponentComponent,
+    PostListItemComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/post-list-item-component/post-list-item-component.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-item-component',
+  templateUrl: './post-list-item-component.component.html',
+  styleUrls: ['./post-list-item-component.component.css']
+})
+export class PostListItemComponentComponent implements OnInit {
+
+	@Input() passingThis: string
+
+  constructor() { }
+
+	post = {
+		title: 'Titre',
+		content: 'Intellectum est enim mihi quidem in multis, et maxime in me ipso, sed paulo ante in omnibus, cum M. Marcellum senatui reique publicae concessisti, commemoratis praesertim offensionibus, te auctoritatem huius ordinis dignitatemque rei publicae tuis vel doloribus vel suspicionibus anteferre. Ille quidem fructum omnis ante actae vitae hodierno die maximum cepit, cum summo consensu senatus, tum iudicio tuo gravissimo et maximo. Ex quo profecto intellegis quanta in dato beneficio sit laus, cum in accepto sit tanta gloria.',
+		date: new Date(),
+	}
+
+  ngOnInit() {
+  }
+
+}
